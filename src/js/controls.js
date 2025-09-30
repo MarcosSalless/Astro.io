c1a727d78da7fc88040e9a4e10eceed8bee25e02
Marcos Gabriel De Souza Salles Dias <est.marcosdias@celepar.pr.gov.br>
2025-09-30 16:41:29 -0300

    [FIX] The 'split' and 'eject' mecanics are fixeds

--- src/js/controls.js
+++ src/js/controls.js
@@ -1,5 +1,6 @@
 import { W, H, UI } from "./config.js";
 import { split, eject, start } from "./gameplay.js";
+import { getMe } from "./world.js";
 
 export const mouse = { x: W / 2, y: H / 2 };
 
@@ -15,4 +16,22 @@ UI.playBtn.addEventListener("click", () => {
 
 UI.nameInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") UI.playBtn.click();
+});
+
+window.addEventListener("keydown", (e) => {
+    if (e.key.toLowerCase() === "w") {
+        const me = getMe();
+        if (me && me.alive && me.cells.length > 0) {
+            eject(me);
+        }
+    }
+});
+
+window.addEventListener("keydown", (e) => {
+    if (e.code === "Space") {
+        const me = getMe();
+        if (me && me.alive && me.cells.length > 0) {
+            split(me);
+        }
+    }
 });
